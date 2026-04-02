cafb092a42764a9e3a9e8f9de7d247b08da7f08e
demosan990 <demosan990@gmail.com>
2026-04-02 19:25:45 +0900

    Fix: Dynamic Supabase Init & Content Table Sync

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = async (req, res) => {
+  res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+  });
+};
